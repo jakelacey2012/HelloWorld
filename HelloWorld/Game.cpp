b5189aa996fd4ce7ca65ed0d32eefdab26c54b9b
Jake Lacey <jakewlacey@gmail.com>
2020-12-08 19:26:58 +0000

    updating thigns

--- HelloWorld/Game.cpp
+++ HelloWorld/Game.cpp
@@ -3,18 +3,47 @@
 #include "Log.h"
 #include "Game.h"
 
+class Component {
+public:
+	virtual void Update();
+	virtual void Draw();
+};
+
+class Entity {
+public:
+	Component *components[100];
+};
+
+class Thing: public Component {
+	Logger* logger;
+public:
+	void SetLogger(Logger* l) {
+		logger = l;
+	}
+	void Update(/*needs access to other players*/) { // needs access to input to be able to change certain things
+		logger->Info("UPDATING_PLAYER");
+		// a thing will need access to all the things to know where they area
+		// 
+	}
+	void Draw() { // needs access to something and draw it on screen
+		logger->Info("DRAWING_PLAYER");
+	}
+};
+
 void Game::Init()
 {
 	Logger logger = CreateLogger();
 	Window window = CreateWindow(&logger);
 
-	while (true) {
-		// input - where do we get input from?
-		// update - what are we updating?
-		// render - what are we drawing?
-	}
+	Entity player;
+	Thing thing;
+	thing.SetLogger(&logger);
+	player.components[0] = &thing;
 
-	window.Draw();
+	while (true) {
+		player.components[0]->Update();
+		player.components[0]->Draw();
+	}
 }
 
 Logger Game::CreateLogger()
